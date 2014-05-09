04dd2de650e5034b3b8c11e23170ef9b632e78f9
Orlando <Orlando@pocketgems.com>
2014-05-08 18:17:42 -0700

    [PubNub_Android] Adding PubNub's iOS bridge classes

---
+++ Android/Callbacks_Android.h
@@ -0,0 +1,54 @@
+//
+//  Callbacks_Android.h
+//  testapp
+//
+//  Created by Ravi Agarwal on 3/19/14.
+//  Copyright (c) 2014 Pocket Gems. All rights reserved.
+//
+
+#ifdef PGDROID
+
+#import <BridgeKit/JavaObject.h>
+#import <BridgeKit/AndroidActivity.h>
+
+#import "PNStructures.h"
+#import "PubNub.h"
+
+
+@interface MessageProcessingCallback : JavaObject
+
+- (id)initWithMessageProcessingBlock:(PNClientMessageProcessingBlock)handlerBlock andDelegate:(PubNub *)pubnubDelegate;
+
+@end
+
+
+
+@interface MessageHistoryProcessingCallback : JavaObject
+
+- (id)initWithHistoryLoadHandlingBlock:(PNClientHistoryLoadHandlingBlock)handlerBlock andDelegate:(PubNub *)pubnubDelegate;
+
+@end
+
+
+
+@interface ChannelSubscriptionCallback : JavaObject
+
+- (id)initWithChannelSubscriptionBlock:(PNClientChannelSubscriptionHandlerBlock)handlerBlock
+                           andDelegate:(PubNub *)pubnubDelegate;
+
+- (void)removeFromSubscriptionCallbackList:(NSString *)channelName;
+
+@end
+
+
+
+@interface ChannelUnsubscriptionCallback : JavaObject
+
+- (id)initWithChannelUnsubscriptionBlock:(PNClientChannelUnsubscriptionHandlerBlock)handlerBlock
+                             andDelegate:(PubNub *)pubnubDelegate;
+
+- (void)removeFromUnsubscriptionCallbackList:(NSString *)channelName;
+
+@end
+
+#endif
---
+++ Android/PubNub_AndroidBridge.h
@@ -0,0 +1,43 @@
+//
+//  PubNub_AndroidBridge.h
+//  PGEngine
+//
+//  Created by Ravi Agarwal on 3/18/14.
+//  Copyright (c) 2014 Pocket Gems. All rights reserved.
+//
+
+#ifdef PGDROID
+
+#import <BridgeKit/JavaObject.h>
+#import <BridgeKit/AndroidActivity.h>
+
+#import "Callbacks_Android.h"
+
+@interface PubNub_AndroidBridge : JavaObject
+
++ (void)initializeJava;
+
++ (void)_initializePubnubWithPublishKey:(NSString *)publishKey
+                           subscribeKey:(NSString *)subscriptionKey
+                              secretKey:(NSString *)secretKey;
+
++ (void)_setClientIdentifier:(NSString *)clientIdntifier;
+
++ (NSString *)_getClientIdentifier;
+
++ (void)_subscribeOnChannel:(NSString *)channel withCallback:(ChannelSubscriptionCallback *)callback;
+
++ (void)_unsubscribeFromChannel:(NSString *)channnel withCallback:(ChannelUnsubscriptionCallback *)callback;
+
++ (void)_sendMessage:(NSString *)messageString channel:(NSString *)channelName withCallback:(MessageProcessingCallback *)callback;
+
++ (void)_requestHistoryForChannel:(NSString *)channelName
+                            count:(int)limit
+                   reverseHistory:(BOOL)shouldReverseHistory
+                     withCallback:(MessageHistoryProcessingCallback *)callback;
+
++ (void)_shutdownPubnub;
+
+@end
+
+#endif
