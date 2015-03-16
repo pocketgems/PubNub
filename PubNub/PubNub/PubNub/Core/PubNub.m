ad34f133e888c2d71c1db1543c334eaca759784c
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2015-03-16 12:24:37 -0700

    Not sending nil errors on connection problems.

--- PubNub/PubNub/PubNub/Core/PubNub.m
+++ PubNub/PubNub/PubNub/Core/PubNub.m
@@ -1619,7 +1619,7 @@ shouldObserveProcessing:(BOOL)shouldObserveProcessing;
 
                                                 postponeConnectionTillNetworkCheck = YES;
 
-                                                [self handleConnectionErrorOnNetworkFailureWithError:nil];
+                                                [self handleConnectionErrorOnNetworkFailure];
                                                 self.asyncLockingOperationInProgress = NO;
 
                                                 [self.observationCenter checkSubscribedOnClientStateChange:self
