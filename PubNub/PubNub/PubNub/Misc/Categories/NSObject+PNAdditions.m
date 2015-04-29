2169fe83e9c42a1e06f954c10ffb3c4d09d32c47
Matthew Koontz <mkoontz@pocketgems.com>
2015-04-29 14:27:00 -0700

    More safely released private queue

--- PubNub/PubNub/PubNub/Misc/Categories/NSObject+PNAdditions.m
+++ PubNub/PubNub/PubNub/Misc/Categories/NSObject+PNAdditions.m
@@ -115,9 +115,10 @@ void objectReleaseCallBack( void *info ) {
 }
 
 - (void)pn_destroyPrivateDispatchQueue {
-    
-    [PNDispatchHelper release:[self pn_privateQueue]];
+
+    dispatch_queue_t dispatchQueue = [self pn_privateQueue];
     objc_setAssociatedObject(self, "privateQueue", nil, OBJC_ASSOCIATION_RETAIN);
+    [PNDispatchHelper release:dispatchQueue];
 }
 
 - (void)pn_dispatchBlock:(dispatch_block_t)block {
