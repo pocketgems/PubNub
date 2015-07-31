c13cc14fb0028308aa461b1a419eac7dc724ea72
Matthew Koontz <mkoontz@pocketgems.com>
2015-07-31 13:00:02 -0700

    Changed the file name of logs

--- PubNub/Misc/Logger/PNLogFileManager.m
+++ PubNub/Misc/Logger/PNLogFileManager.m
@@ -5,31 +5,38 @@
  */
 #import "PNLogFileManager.h"
 
+@interface PNLogFileManager()
+
+@property (nonatomic, readwrite, strong) NSDateFormatter *dateFormatter;
+
+@end
 
 #pragma mark Interface implementation
 
 @implementation PNLogFileManager
 
 - (instancetype)init {
-    
     // Configure file manager with default storage in application's Documents folder.
     NSArray *documents = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     
-    return [self initWithLogsDirectory:[documents lastObject]];
+    if (self = [self initWithLogsDirectory:[documents lastObject]]) {
+        NSString *dateFormat = @"yyyy'-'MM'-'dd'T'HH'-'mm'-'ss'";
+        _dateFormatter = [[NSDateFormatter alloc] init];
+        [_dateFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"]];
+        [_dateFormatter setDateFormat:dateFormat];
+        [_dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    }
+    return self;
 }
 
 - (NSString *)newLogFileName {
-    
-    return [[super newLogFileName] stringByReplacingOccurrencesOfString:@".log"
-                                                             withString:@".txt"];
+    NSString *formattedDate = [self.dateFormatter stringFromDate:[NSDate date]];
+    return [[NSString alloc] initWithFormat:@"pubnub-console-dump-%@.log", formattedDate];
 }
 
 - (BOOL)isLogFile:(NSString *)fileName {
-    
-    NSString *originalName = [fileName stringByReplacingOccurrencesOfString:@".txt"
-                                                                 withString:@".log"];
-    
-    return [super isLogFile:originalName];
+    return [fileName hasPrefix:@"pubnub-console-dump-"]
+        && [fileName hasSuffix:@".log"];
 }
 
 #pragma mark -
