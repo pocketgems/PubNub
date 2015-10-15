adc5535f18f6460f814754e8016f3396ed617e67
Matthew Koontz <mkoontz@pocketgems.com>
2015-10-14 19:15:00 -0700

    Handle subscribe timeouts as success

--- PubNub/Data/Managers/PNSubscriber.m
+++ PubNub/Data/Managers/PNSubscriber.m
@@ -859,6 +859,8 @@ typedef NS_OPTIONS(NSUInteger, PNSubscriberState) {
     if (!status.isError && status.category != PNCancelledCategory) {
         
         [self handleSuccessSubscriptionStatus:status];
+    } else if (status.isError && status.category == PNTimeoutCategory) {
+        [self handleSuccessSubscriptionStatus:status];
     }
     else {
         
